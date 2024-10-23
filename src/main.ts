47b7018d9bd12a67da7d2e3a73fb42f40e87207f
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-10-23 13:14:26 +0900

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.mount("#app");
+
+app.config.errorHandler = (error) => {
+  console.error(error);
+};

6bfe80f2fa76d4285d5a8d19013f2ed7556d255c
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-10-24 14:45:12 +0900

    chore: update

---
+++ src/presentation/widgets/index.ts
@@ -0,0 +1,6 @@
+import TodoList, { type TodoListProps } from "./TodoList/TodoList";
+import Loading, { type LoadingProps } from "./Loading/Loading.vue";
+
+export { Loading, TodoList };
+
+export type { TodoListProps, LoadingProps };

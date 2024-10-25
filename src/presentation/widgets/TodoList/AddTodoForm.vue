84d5423eb3bee090125fadfeb56bd25cd7874868
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-10-25 20:38:01 +0900

    chore: add source

--- src/presentation/widgets/TodoList/AddTodoForm.vue
+++ src/presentation/widgets/TodoList/AddTodoForm.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { reactive, defineProps, type FormHTMLAttributes } from 'vue';
-import { useForm, Field } from 'vee-validate';
+import { reactive, defineProps, type FormHTMLAttributes } from "vue";
+import { useForm, Field } from "vee-validate";
 import * as Validator from "../../../validator";
-import { toTypedSchema } from '@vee-validate/zod';
+import { toTypedSchema } from "@vee-validate/zod";
 
 export type AddTodoFormProps = {
   onSubmit: (data: { title: string }) => void;
@@ -19,14 +19,10 @@ const methods = useForm<Validator.AddTodoForm>({
 });
 
 const formProps: FormHTMLAttributes = {
-  onSubmit: (event) => {
-    event.preventDefault();
-    methods.handleSubmit((fields) => {
-      props.onSubmit(fields);
-      methods.resetForm();
-      console.log(fields)
-    })
-  },
+  onSubmit: methods.handleSubmit((fields) => {
+    props.onSubmit(fields);
+    methods.resetForm();
+  }),
 };
 
 const [title, titleProps] = methods.defineField("title");
